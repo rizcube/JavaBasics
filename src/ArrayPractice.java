fbc784b0f2d843a45fa0026459bc2513f304f9c3
rizcube <rizcubeservices@gmail.com>
2020-05-03 16:22:12 +0100

    Simple array practice

---
+++ src/ArrayPractice.java
@@ -0,0 +1,22 @@
+
+public class ArrayPractice {
+
+	public static void main(String args []) {
+		
+		String[] studentNames = new String[] {"John", "David", "Henry", "Bob", "Victor"};
+		int[] studentAge = new int[] {40,30,29,44,38};
+		
+		String[] colleagueNames = new String[] {"Sal", "Laura", "Nikki", "Sharon", "Sunaina"};
+		String[] subject = new String[] {"ICT", "Business studies", "Social sciences", "Psychology","Social science"};
+		
+		
+		System.out.println(studentNames[4]);
+		System.out.println(studentAge[4]);
+	
+		System.out.println(colleagueNames[2]);
+		System.out.println(subject[2]);
+		
+	}
+	
+	
+}
