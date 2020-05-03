ba5bff0896b9effd5679710c9b32a87163b8a135
rizcube <rizcubeservices@gmail.com>
2020-05-03 12:04:49 +0100

    updated switch class

--- src/Switch.java
+++ src/Switch.java
@@ -2,7 +2,7 @@ public class Switch {
 
 	public static void main(String arg[]) {
 	
-		String grade = "A-";  // students grade
+		String grade = "F";  // students grade
 		switch(grade) {
 		case "A+":
 			System.out.println("Great student");
@@ -13,17 +13,23 @@ public class Switch {
 		case "A-":
 			System.out.println("Great student");
 			break;
-		case "B+":	case "B":	case "B-":
+		case "B+":	case "B":	
+		case "B-":
 			System.out.println("Good student");
 			break;
-		case "C+":	case "C":	case "C-":	
+		case "C+":	case "C":	
+		case "C-":	
 			System.out.println("Poor student");
 			break;
-		case "D+":	case "D":	case "D-":
+		case "D+":	case "D":	
+		case "D-":
 			System.out.println("very poor student");
 			break;
+		case "F":
+			System.out.println("Failed student");
+			break;
 		default:
-			System.out.println("Failed");
+			System.out.println("Invalid grade");
 			break;
 		}
 	}
