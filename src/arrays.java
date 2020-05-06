bbeccdb102ff55d28b45c7ff6237bdee8bfc32f8
rizcube <rizcubeservices@gmail.com>
2020-05-06 11:31:51 +0100

    array

---
+++ src/arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+
+	public static void main(String args[]) {
+		
+		String[] studentNames;
+		studentNames = new String[] {"A", "B", "C"};
+		System.out.println(studentNames[1]);
+		
+		int[] studentAge;
+		studentAge = new int[] {2,4,6};
+		System.out.println(studentAge[1]);
+		
+	}
+}
