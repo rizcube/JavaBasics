ba3a1994489abb217f1f9cd5097b5bf0e0348df5
rizcube <rizcubeservices@gmail.com>
2020-05-09 12:14:19 +0100

    creating a method that returns double value

---
+++ src/doubleMethod.java
@@ -0,0 +1,13 @@
+// Creating a method that return a double value
+public class doubleMethod {
+
+	public static void main(String args[]) {
+		System.out.println(add(54,46));
+	}
+	
+	public static Double add(double num1, double num2) {
+		double sum = num1 + num2;
+		return sum;		
+	}
+	
+}
