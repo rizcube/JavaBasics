1b83d9d524c367351934203248d02785d18feba3
rizcube <rizcubeservices@gmail.com>
2020-05-14 12:12:42 +0100

    created a class student and someMethods name_of_student, grade, level

---
+++ src/instanceMethod.java
@@ -0,0 +1,39 @@
+// creating and learning the use of instance method
+/*  
+ * Create a void method name_of_student, this method takes name and prints ("My name is :" name )
+ * create a void method grade, this mehtod takes double value and prints ("your grade is :, grade)
+ */
+class Student {	
+
+	public void name_of_student(String name) {
+		//System.out.println("Student name is :"+ name);
+		System.out.println(String.format("Student name is : %s", name));
+	}
+	
+	public void grade(double grade) {
+		//System.out.println("Student grade is" + grade);
+		System.out.println(String.format("Student grade is : %f", grade));
+	}
+	
+	public void student_level(int level) {
+		System.out.println(String.format("You are in level %d", level));
+	}
+	
+}
+public class instanceMethod {
+	
+	public static void main(String args[]) {
+		// create student1 object
+		// class name  -  Object name =  new Classname
+		Student student1 = new Student();
+		student1.name_of_student("Riz");
+		student1.grade(4.25);
+		student1.student_level(9);			
+	
+		Student student2 = new Student();
+		student2.name_of_student("Eeshal");
+		student2.grade(3.65);
+		student2.student_level(7);
+	}
+		
+}
--- src/stringInterpolation.java
+++ src/stringInterpolation.java
@@ -8,11 +8,12 @@ public class stringInterpolation {
 		String state = "Outer space";
 		int zipcode = 786;
 		String country = "Heaven";
+		double grade = 4.0;
 		
 		System.out.println(String.format("My name is %s", custName));
 		
-		String custDetails = "Customer details %s, StreetName: %s, Age: %d, City: %s, State: %s, Zipcode: %d, Country: %s";
-		System.out.println(String.format(custDetails, custName, streetName, age, city, state, zipcode, country));
+		String custDetails = "Customer details %s, StreetName: %s, Grade %f ,Age: %d, City: %s, State: %s, Zipcode: %d, Country: %s";
+		System.out.println(String.format(custDetails, custName, streetName,grade, age, city, state, zipcode, country));
 		
 		/*
 		String template = "%s is from %s and is %s years old.";
